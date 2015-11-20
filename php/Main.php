948b74b51b9d45bbfaba0a0b77f0bb47f0b9c865
Gert <Gert-dev@users.noreply.github.com>
2015-11-20 21:39:58 +0100

    Main - Reenable the error handler.

--- php/Main.php
+++ php/Main.php
@@ -4,7 +4,7 @@ use PhpIntegrator\Config;
 use PhpIntegrator\ErrorHandler;
 
 require_once(__DIR__ . '/ErrorHandler.php');
-// ErrorHandler::register();
+ErrorHandler::register();
 
 require_once(__DIR__ . '/Config.php');
 
