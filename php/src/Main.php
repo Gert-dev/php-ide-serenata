d5f727509d91a541783b9d42f90dfa2fb32199b2
Gert <Gert-dev@users.noreply.github.com>
2016-01-09 16:46:30 +0100

    Main - Set the xdebug limit to -1 as it can pose problems with large code bases in php-parser's NameResolver.

--- php/src/Main.php
+++ php/src/Main.php
@@ -2,6 +2,10 @@
 
 use PhpIntegrator\Application;
 
+// This limit can pose a problem for NameResolver built-in to php-parser (it can go over a nesting level of 300 in e.g.
+// a Symfony2 code base).
+ini_set('xdebug.max_nesting_level', -1);
+
 chdir(__DIR__);
 
 require '../vendor/autoload.php';
