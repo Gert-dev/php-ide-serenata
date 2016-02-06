1c7247f5e84d699b35efe5d296757dadc67ba94d
Gert <Gert-dev@users.noreply.github.com>
2016-02-06 20:37:16 +0100

    Indexer - Split type resolving off into a separate class for reusability.

--- php/src/Indexer.php
+++ php/src/Indexer.php
@@ -1205,51 +1205,6 @@ class Indexer
     }
 
     /**
-     * Returns a boolean indicating if the specified value is a class type or not.
-     *
-     * @param string $type
-     *
-     * @return bool
-     */
-    protected function isClassType($type)
-    {
-        return ucfirst($type) === $type && $type !== '$this';
-    }
-
-    /**
-     * Retrieves the sole class name from the specified return value statement.
-     *
-     * @example "null" returns null.
-     * @example "FooClass" returns "FooClass".
-     * @example "FooClass|null" returns "FooClass".
-     * @example "FooClass|BarClass|null" returns null (there is no single type).
-     *
-     * @param string $returnValueStatement
-     *
-     * @return string|null
-     */
-    protected function getSoleClassName($returnValueStatement)
-    {
-        if ($returnValueStatement) {
-            $types = explode(DocParser::TYPE_SPLITTER, $returnValueStatement);
-
-            $classTypes = [];
-
-            foreach ($types as $type) {
-                if ($this->isClassType($type)) {
-                    $classTypes[] = $type;
-                }
-            }
-
-            if (count($classTypes) === 1) {
-                return $classTypes[0];
-            }
-        }
-
-        return null;
-    }
-
-    /**
      * Resolves and determines the FQSEN of the specified type.
      *
      * @param string                                   $type
@@ -1261,51 +1216,12 @@ class Indexer
         $type,
         Indexer\UseStatementFetchingVisitor $useStatementFetchingVisitor = null
     ) {
-        if (empty($type)) {
-            return null;
-        }
+        $typeResolver = new TypeResolver(
+            $useStatementFetchingVisitor ? $useStatementFetchingVisitor->getNamespace() : null,
+            $useStatementFetchingVisitor ? $useStatementFetchingVisitor->getUseStatements() : []
+        );
 
-        $soleClassName = $this->getSoleClassName($type);
-
-        if (!empty($soleClassName)) {
-            if ($soleClassName[0] !== "\\" && $useStatementFetchingVisitor) {
-
-                $soleClassNameParts = explode('\\', $soleClassName);
-
-                foreach ($useStatementFetchingVisitor->getUseStatements() as $use) {
-                    if ($use['alias'] === $soleClassNameParts[0]) {
-                        array_shift($soleClassNameParts);
-
-                        $fullName = $use['fqsen'];
-
-                        if (!empty($soleClassNameParts)) {
-                            /*
-                             * This block is only executed when relative names are used with more than one part, i.e.:
-                             *   use A\B\C;
-                             *
-                             *   C\D::foo();
-                             *
-                             * 'C' will be dropped from 'C\D', and the remaining 'D' will be appended to 'A\B\C',
-                             * becoming 'A\B\C\D'.
-                             */
-                            $fullName .= '\\' . implode('\\', $soleClassNameParts);
-                        }
-
-                        return $fullName;
-                    }
-                }
-
-                // Still here? There must be no explicit use statement, default to the current namespace.
-                $fullName = $useStatementFetchingVisitor->getNamespace() ?: '';
-                $fullName .= '\\' . $soleClassName;
-
-                return $fullName;
-            }
-
-            return $soleClassName;
-        }
-
-        return $type;
+        return $typeResolver->getFullTypeForDocblockType($type);
     }
 
     /**
