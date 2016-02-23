2c8b23d8c7c92471fef04430621d8da219113d87
Gert <Gert-dev@users.noreply.github.com>
2016-02-23 19:20:29 +0100

    Main - Enable error_reporting so we can properly see what is going wrong in setups where users have disabled it on a global level.

--- php/src/Main.php
+++ php/src/Main.php
@@ -2,6 +2,9 @@
 
 use PhpIntegrator\Application;
 
+// Show us pretty much everything so we can properly debug what is going wrong.
+error_reporting(E_ALL & ~E_DEPRECATED);
+
 // This limit can pose a problem for NameResolver built-in to php-parser (it can go over a nesting level of 300 in e.g.
 // a Symfony2 code base). Also, -1 as a value doesn't work in some setups, see also:
 // https://github.com/Gert-dev/php-integrator-base/issues/91
