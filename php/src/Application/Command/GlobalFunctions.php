c228fca7f70a2999581a7fc49b78b24aa6dd3c63
Gert <Gert-dev@users.noreply.github.com>
2016-02-27 19:45:55 +0100

    GlobalFunctions - Refactor for testability.

--- php/src/Application/Command/GlobalFunctions.php
+++ php/src/Application/Command/GlobalFunctions.php
@@ -16,14 +16,24 @@ class GlobalFunctions extends BaseCommand
     /**
      * @inheritDoc
      */
-    protected function process(ArrayAccess $arguments)
-    {
-        $result = [];
+     protected function process(ArrayAccess $arguments)
+     {
+         $result = $this->getGlobalConstants();
 
-        foreach ($this->indexDatabase->getGlobalFunctions() as $function) {
-            $result[$function['name']] = $this->getIndexDataAdapter()->getFunctionInfo($function);
-        }
+         return $this->outputJson(true, $result);
+     }
 
-        return $this->outputJson(true, $result);
-    }
+     /**
+      * @return array
+      */
+     protected function getGlobalConstants()
+     {
+         $result = [];
+
+         foreach ($this->indexDatabase->getGlobalFunctions() as $function) {
+             $result[$function['name']] = $this->getIndexDataAdapter()->getFunctionInfo($function);
+         }
+
+         return $result;
+     }
 }
