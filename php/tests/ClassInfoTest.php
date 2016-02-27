1266cb3887cca8218b2046bff2980db24558b21f
Gert <Gert-dev@users.noreply.github.com>
2016-02-27 20:02:06 +0100

    Tests - Stubbed a test for the class info command.

---
+++ php/tests/ClassInfoTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PhpIntegrator;
+
+use ReflectionClass;
+
+use PhpIntegrator\Application\Command;
+
+class ClassInfoTest extends IndexedTest
+{
+    public function testGlobalConstants()
+    {
+        // TODO
+    }
+}
