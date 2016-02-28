09f89687dc57cda3125e9606bc3f3f732e523d3f
Gert <Gert-dev@users.noreply.github.com>
2016-02-28 18:21:02 +0100

    Tests - Added a test for the class info command.

--- php/tests/Application/Command/ClassInfoTest.php
+++ php/tests/Application/Command/ClassInfoTest.php
@@ -2,14 +2,69 @@
 
 namespace PhpIntegrator\Application\Command;
 
-use ReflectionClass;
-
 use PhpIntegrator\IndexedTest;
 
 class ClassInfoTest extends IndexedTest
 {
-    public function testClassInfo()
+    protected function getClassInfo($file, $fqsen)
     {
-        // TODO
+        $path = $this->getPathFor($file);
+
+        $indexDatabase = $this->getDatabaseForTestFile($path);
+
+        $command = new ClassInfo();
+        $command->setIndexDatabase($indexDatabase);
+
+        return $command->getClassInfo($fqsen);
+    }
+
+    protected function getPathFor($file)
+    {
+        return __DIR__ . '/ClassInfoTest/' . $file;
+    }
+
+    /**
+     * @expectedException \UnexpectedValueException
+     */
+    public function testFailsOnUnknownClass()
+    {
+        $output = $this->getClassInfo('SimpleClass.php', 'DoesNotExist');
+    }
+
+    public function testDataIsCorrectForASimpleClass()
+    {
+        $fileName = 'SimpleClass.php';
+
+        $output = $this->getClassInfo($fileName, 'A\SimpleClass');
+
+        $this->assertEquals($output, [
+            'name'               => 'A\SimpleClass',
+            'startLine'          => 5,
+            'endLine'            => 8,
+            'shortName'          => 'SimpleClass',
+            'filename'           => $this->getPathFor($fileName),
+            'type'               => 'class',
+            'isAbstract'         => false,
+            'isBuiltin'          => false,
+            'isDeprecated'       => false,
+
+            'descriptions'       => [
+                'short' => null,
+                'long'  => null
+            ],
+
+            'parents'            => [],
+            'interfaces'         => [],
+            'traits'             => [],
+            'directParents'      => [],
+            'directInterfaces'   => [],
+            'directTraits'       => [],
+            'directChildren'     => [],
+            'directImplementors' => [],
+            'directTraitUsers'   => [],
+            'constants'          => [],
+            'properties'         => [],
+            'methods'            => [],
+        ]);
     }
 }
