e02e5ca2c9449a137ca9cc11d1523eb4a6bfbd23
Gert <Gert-dev@users.noreply.github.com>
2016-04-16 19:44:49 +0200

    Fix vertical alignment.

--- php/src/Sql/1-structural_elements.sql
+++ php/src/Sql/1-structural_elements.sql
@@ -77,7 +77,7 @@ CREATE TABLE structures(
     start_line                 integer unsigned,
     end_line                   integer unsigned,
 
-    structure_type_id integer NOT NULL,
+    structure_type_id          integer NOT NULL,
     short_description          text,
     long_description           text,
     is_builtin                 tinyint(1) NOT NULL DEFAULT 0,
@@ -148,12 +148,12 @@ CREATE TABLE structures_traits_linked(
 CREATE TABLE structures_traits_aliases(
     id                         integer NOT NULL PRIMARY KEY AUTOINCREMENT,
 
-    structure_id       integer unsigned NOT NULL,
-    trait_structure_id integer unsigned,
-    access_modifier_id          integer unsigned,
+    structure_id               integer unsigned NOT NULL,
+    trait_structure_id         integer unsigned,
+    access_modifier_id         integer unsigned,
 
-    name                        varchar(255) NOT NULL,
-    alias                       varchar(255),
+    name                       varchar(255) NOT NULL,
+    alias                      varchar(255),
 
     FOREIGN KEY(structure_id) REFERENCES structures(id)
         ON DELETE CASCADE
@@ -172,10 +172,10 @@ CREATE TABLE structures_traits_aliases(
 CREATE TABLE structures_traits_precedences(
     id                         integer NOT NULL PRIMARY KEY AUTOINCREMENT,
 
-    structure_id       integer unsigned NOT NULL,
-    trait_structure_id integer unsigned NOT NULL,
+    structure_id               integer unsigned NOT NULL,
+    trait_structure_id         integer unsigned NOT NULL,
 
-    name                        varchar(255) NOT NULL,
+    name                       varchar(255) NOT NULL,
 
     FOREIGN KEY(structure_id) REFERENCES structures(id)
         ON DELETE CASCADE
@@ -207,7 +207,7 @@ CREATE TABLE functions(
     return_description    text,
 
     -- Specific to members.
-    structure_id integer unsigned,
+    structure_id          integer unsigned,
     access_modifier_id    integer unsigned,
 
     is_magic              tinyint(1),
@@ -288,7 +288,7 @@ CREATE TABLE properties(
     full_return_type      varchar(255),
     return_description    text,
 
-    structure_id integer unsigned NOT NULL,
+    structure_id          integer unsigned NOT NULL,
     access_modifier_id    integer unsigned NOT NULL,
 
     is_magic              tinyint(1) NOT NULL DEFAULT 0,
