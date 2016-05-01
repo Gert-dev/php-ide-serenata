dbabbd2a5271cfe1fbd6092b6d1c80f81b5e068e
Gert <Gert-dev@users.noreply.github.com>
2016-05-01 19:58:30 +0200

    Fix the order of assertion arguments.

--- php/tests/Application/Command/ResolveTypeTest.php
+++ php/tests/Application/Command/ResolveTypeTest.php
@@ -16,13 +16,13 @@ class ResolveTypeTest extends IndexedTest
         $command = new ResolveType();
         $command->setIndexDatabase($indexDatabase);
 
-        $this->assertEquals($command->resolveType('C', $path, 1), 'C');
-        $this->assertEquals($command->resolveType('C', $path, 5), 'A\C');
-        $this->assertEquals($command->resolveType('C', $path, 10), 'B\C');
-        $this->assertEquals($command->resolveType('DateTime', $path, 10), 'B\DateTime');
-        $this->assertEquals($command->resolveType('DateTime', $path, 11), 'DateTime');
-        $this->assertEquals($command->resolveType('DateTime', $path, 12), 'DateTime');
-        $this->assertEquals($command->resolveType('D\Test', $path, 13), 'C\D\Test');
+        $this->assertEquals('C', $command->resolveType('C', $path, 1));
+        $this->assertEquals('A\C', $command->resolveType('C', $path, 5));
+        $this->assertEquals('B\C', $command->resolveType('C', $path, 10));
+        $this->assertEquals('B\DateTime', $command->resolveType('DateTime', $path, 10));
+        $this->assertEquals('DateTime', $command->resolveType('DateTime', $path, 11));
+        $this->assertEquals('DateTime', $command->resolveType('DateTime', $path, 12));
+        $this->assertEquals('C\D\Test', $command->resolveType('D\Test', $path, 13));
     }
 
     /**
