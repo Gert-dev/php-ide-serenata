51e0fe3ded53323942a5266e2e0b1ca33a122f0e
Gert <Gert-dev@users.noreply.github.com>
2016-08-28 20:58:23 +0200

    Update documentation

--- php/src/Analysis/CircularDependencyException.php
+++ php/src/Analysis/CircularDependencyException.php
@@ -5,7 +5,8 @@ namespace PhpIntegrator\Analysis;
 use Exception;
 
 /**
- *
+ * Indicates a circular dependency between classlikes (i.e. a class extending itself or an interface implementing
+ * itself).
  */
 class CircularDependencyException extends Exception
 {
