e77e8fdc3df138d7c0526ac15728689c8a546a26
Gert <Gert-dev@users.noreply.github.com>
2016-09-03 19:17:18 +0200

    Refactor the IndexedTest

--- php/src/Test/IndexedTest.php
+++ php/src/Test/IndexedTest.php
@@ -29,28 +29,39 @@ abstract class IndexedTest extends \PHPUnit_Framework_TestCase
         return $refMethod->invoke($app);
     }
 
-    protected function getDatabaseForTestFile($testPath, $mayFail = false)
+    protected function getDatabase($indexBuiltinItems = false)
     {
         $indexDatabase = new IndexDatabase(':memory:', 1);
 
-        // Indexing these on every test majorly slows down testing. Instead, we simply don't rely on PHP's built-in
-        // structural elements during testing.
-        $indexDatabase->insert(IndexStorageItemEnum::SETTINGS, [
-            'name'  => 'has_indexed_builtin',
-            'value' => 1
-        ]);
+        if (!$indexBuiltinItems) {
+            // Indexing these on every test majorly slows down testing. Instead, we simply don't rely on PHP's built-in
+            // structural elements during testing.
+            $indexDatabase->insert(IndexStorageItemEnum::SETTINGS, [
+                'name'  => 'has_indexed_builtin',
+                'value' => 1
+            ]);
+        }
+
+        return $indexDatabase;
+    }
+
+    protected function getDatabaseForTestFile($testPath = null, $mayFail = false)
+    {
+        $indexDatabase = $this->getDatabase(false);
 
         $reindexCommand = new Command\ReindexCommand($this->getParser(), null, $indexDatabase);
 
-        $success = $reindexCommand->reindex(
-            [$testPath],
-            false,
-            false,
-            false
-        );
+        if ($testPath) {
+            $success = $reindexCommand->reindex(
+                [$testPath],
+                false,
+                false,
+                false
+            );
 
-        if (!$mayFail) {
-            $this->assertTrue($success);
+            if (!$mayFail) {
+                $this->assertTrue($success);
+            }
         }
 
         return $indexDatabase;
